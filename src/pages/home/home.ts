052471d86f8d8bdbb0b7d045dde8be8440d24334
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2017-11-22 17:51:40 +0100

    Updated

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Platform, NavController } from 'ionic-angular';
 
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 
-import { NetworkConnectionProvider } from '../../providers/network-connection/network-connection'
+import { NetworkConnectionProvider } from '../../providers/network-connection/network-connection';
 
 @Component({
   selector: 'page-home',
@@ -15,27 +15,33 @@ export class HomePage {
 
   private readonly tawkChatLink : string = 'https://tawk.to/chat/5a144685bb0c3f433d4ca72e/default/?$_tawk_popout=true';
 
-  constructor(public navCtrl: NavController, private iab: InAppBrowser, private networkConnection: NetworkConnectionProvider) {
-    console.log("constructor home.ts");
+  constructor(public navCtrl: NavController, private iab: InAppBrowser, private networkConnection: NetworkConnectionProvider,
+    private platform: Platform) {
+  }
 
-    /*
-    networkConnection.watchNetworkConnection().subscribe(() => {
-      console.log('Network connected!');
-      // We just got a connection but we need to wait briefly
-      // before we determine the connection type. Might need to wait.
-      // prior to doing any api requests as well.
-      setTimeout(() => {
-        if (this.networkConnection.getNetwork().type === 'wifi') {
-          console.log('We got a wifi connection, woohoo!');
-        }
-      }, 3000);
-    });
+  ionViewDidEnter() {
+    console.log("ionViewDidEnter");
+    this.platform.ready().then((readySource) => {
+      console.log('Platform ready from', readySource);
+      // Platform now ready, execute any required native code.
 
-    networkConnection.watchNetworkDisconnect().subscribe(() => {
-      console.log('Network was disconnected :-(');
+      this.networkConnection.watchNetworkConnection().subscribe(() => {
+        console.log('Network connected!');
+        // We just got a connection but we need to wait briefly
+        // before we determine the connection type. Might need to wait.
+        // prior to doing any api requests as well.
+        setTimeout(() => {
+          if (this.networkConnection.getNetwork().type === 'wifi') {
+            console.log('We got a wifi connection, woohoo!');
+          }
+        }, 3000);
+      });
+  
+      this.networkConnection.watchNetworkDisconnect().subscribe(() => {
+        console.log('Network was disconnected :-(');
+      });
+
     });
-    */
-    
   }
 
   onClickChat() {
