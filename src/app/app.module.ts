bdd81c5d98530423060bc877960ec55b3856cde3
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2017-11-22 11:52:51 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +26,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    InAppBrowser,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private readonly tawkChatLink : string = 'https://tawk.to/chat/xxxxxxxxxxxxxxxxxxxxxxxx/default/?$_tawk_popout=true';
 
+  constructor(public navCtrl: NavController, private iab: InAppBrowser) {
+
+  }
+
+  onClickChat() {
+    const browser = this.iab.create(this.tawkChatLink,'_self',{location:'no'}); 
   }
 
 }
