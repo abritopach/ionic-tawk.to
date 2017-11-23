5faa843f4e4b6ee4dbad0a3882d7d7c15524b71a
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2017-11-23 17:36:01 +0100

    Updated check connection functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Platform, NavController } from 'ionic-angular';
+import { Platform, NavController, AlertController } from 'ionic-angular';
 
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 
@@ -11,12 +11,12 @@ import { NetworkConnectionProvider } from '../../providers/network-connection/ne
 })
 export class HomePage {
 
-  isOnline: boolean = false;
+  hasConnection: boolean = false;
 
   private readonly tawkChatLink : string = 'https://tawk.to/chat/5a144685bb0c3f433d4ca72e/default/?$_tawk_popout=true';
 
   constructor(public navCtrl: NavController, private iab: InAppBrowser, private networkConnection: NetworkConnectionProvider,
-    private platform: Platform) {
+    private platform: Platform, private alertCtrl: AlertController) {
   }
 
   ionViewDidEnter() {
@@ -24,31 +24,49 @@ export class HomePage {
     this.platform.ready().then((readySource) => {
       console.log('Platform ready from', readySource);
       // Platform now ready, execute any required native code.
+      if (readySource == "cordova") {
 
-      this.networkConnection.watchNetworkConnection().subscribe(() => {
-        console.log('Network connected!');
-        // We just got a connection but we need to wait briefly
-        // before we determine the connection type. Might need to wait.
-        // prior to doing any api requests as well.
-        setTimeout(() => {
-          if (this.networkConnection.getNetwork().type === 'wifi') {
-            console.log('We got a wifi connection, woohoo!');
-          }
-        }, 3000);
-      });
-  
-      this.networkConnection.watchNetworkDisconnect().subscribe(() => {
-        console.log('Network was disconnected :-(');
-      });
+        if (this.networkConnection.getNetwork().type != "NONE") {
+          this.hasConnection = true;
+        }
+
+        this.networkConnection.watchNetworkConnection().subscribe(() => {
+          console.log('Network connected!');
+          // We just got a connection but we need to wait briefly
+          // before we determine the connection type. Might need to wait.
+          // prior to doing any api requests as well.
+          this.hasConnection = true;
+          setTimeout(() => {
+            if (this.networkConnection.getNetwork().type === 'wifi') {
+              console.log('We got a wifi connection, woohoo!');
+            }
+          }, 3000);
+        });
+    
+        this.networkConnection.watchNetworkDisconnect().subscribe(() => {
+          console.log('Network was disconnected :-(');
+          this.hasConnection = false;
+        });
+      }
 
     });
   }
 
   onClickChat() {
-
-    //if (this.isOnline)
-
-    const browser = this.iab.create(this.tawkChatLink,'_self',{location:'no'}); 
+    if (this.hasConnection) {
+      const browser = this.iab.create(this.tawkChatLink,'_self',{location:'no'}); 
+    }
+    else this.presentAlert();
   }
+
+  presentAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Error Internet Connection',
+      subTitle: 'There is no internet connection. Please check your connection.',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+  
 
 }
